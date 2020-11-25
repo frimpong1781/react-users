1ebc1c759d70f3ce21ac475c3fadc13e918b3e2c
frimpong1781 <frimpong1781@yahoo.com>
2020-11-25 07:15:07 +0000

    Added react store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,14 @@
 import {createStore, combineReducers} from 'redux';
 import usersReducer from '../reducers/usersReducer';
+import authreducer from '../reducers/authReducer';
 
+// for creating single reducer, use the below code
 // export const store = createStore(usersReducer);
 
+// Use code below when creating multiples reducers
 export const store = createStore(
     combineReducers({
-        users: usersReducer
+        users: usersReducer,
+        auth: authreducer
     })
 )
