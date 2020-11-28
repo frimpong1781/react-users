7e12b641a8ab8c865fa25a2bb1f528a59ce08046
frimpong1781 <frimpong1781@yahoo.com>
2020-11-28 16:49:50 +0000

    the switch case ADD_USER  return state was updated

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -21,12 +21,18 @@ const initialState = {
       ],
 };
 
-const usersReducer = (state = {initialState}, action) => {
+// Use the initialState as a default value
+const usersReducer = (state = initialState, action) => {
+  // The reducer normally looks at the action type field to decide what happens
     switch (action.type) {
+      // Do something here based on the different types of actions
         case "ADD_USER":
-                return state;
+            console.log(action.payload)
+            return {...state, users: [...state.users, action.payload]};
     
         default:
+          // If this reducer doesn't recognize the action type, or doesn't
+          // care about this specific action, return the existing state unchanged
             return state;
     }
 }
