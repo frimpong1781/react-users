b216fb001abd2b3a7902385b7c10ab1cebfaea1b
frimpong1781 <frimpong1781@yahoo.com>
2021-01-23 17:24:48 +0000

    add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
+
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCs8EE1zBCEEXPwoFPJmFxvh9kn7AjzLfg",
+    authDomain: "react-users-1dfbb.firebaseapp.com",
+    projectId: "react-users-1dfbb",
+    storageBucket: "react-users-1dfbb.appspot.com",
+    messagingSenderId: "114497622126",
+    appId: "1:114497622126:web:69a833b273b28ba851d67f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
